a209c22ac7b66880cd9bf19c5470e02a3390ea6a
aihridoy <cse_1912020168@lus.ac.bd>
2025-08-30 00:18:50 +0600

    feat: Making first get request using query

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -3,22 +3,17 @@ import { Label } from '@/components/ui/label';
 import { Slider } from '@/components/ui/slider';
 import { Switch } from '@/components/ui/switch';
 import { useToast } from '@/components/ui/use-toast';
+import { useGetPostsQuery } from '@/redux/api/apiSlice';
 import {
   setPriceRange,
   toggleStatus,
 } from '@/redux/features/products/productSlice';
 import { useAppSelector } from '@/redux/hook';
 import { IProduct } from '@/types/globalTypes';
-import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 export default function Products() {
-  const [data, setData] = useState<IProduct[]>([]);
-  useEffect(() => {
-    fetch('./data.json')
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
+  const { data } = useGetPostsQuery('data');
 
   const { toast } = useToast();
 
@@ -32,13 +27,16 @@ export default function Products() {
   let productsData;
 
   if (status) {
-    productsData = data.filter(
-      (item) => item.status === true && item.price < priceRange
+    productsData = data?.data.filter(
+      (item: { status: boolean; price: number }) =>
+        item.status === true && item.price < priceRange
     );
   } else if (priceRange > 0) {
-    productsData = data.filter((item) => item.price < priceRange);
+    productsData = data?.data.filter(
+      (item: { price: number }) => item.price < priceRange
+    );
   } else {
-    productsData = data;
+    productsData = data?.data;
   }
 
   return (
@@ -66,8 +64,8 @@ export default function Products() {
         </div>
       </div>
       <div className="col-span-9 grid grid-cols-3 gap-10 pb-20">
-        {productsData?.map((product) => (
-          <ProductCard product={product} />
+        {productsData?.map((product: IProduct) => (
+          <ProductCard product={product} key={product._id} />
         ))}
       </div>
     </div>
--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000' }),
   endpoints: (build) => ({
     getPosts: build.query({
       query: () => ({ url: '/products' }),
