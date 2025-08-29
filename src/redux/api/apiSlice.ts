7343ff7352a2b83a2122f186dff6f2880f953671
aihridoy <cse_1912020168@lus.ac.bd>
2025-08-30 02:06:21 +0600

    fix: env added

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_BASE_URL }),
   endpoints: (build) => ({
     getProducts: build.query({
       query: () => ({ url: '/products' }),
