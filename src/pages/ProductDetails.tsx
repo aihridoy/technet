29eea552050ecc81f347a84051db37094dbd7a68
aihridoy <cse_1912020168@lus.ac.bd>
2025-09-03 20:14:00 +0600

    Handling-post-method-using-mutation-hook

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -24,7 +24,7 @@ export default function ProductDetails() {
           <Button>Add to cart</Button>
         </div>
       </div>
-      <ProductReview />
+      {id && <ProductReview id={id} />}
     </>
   );
 }
