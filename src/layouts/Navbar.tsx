a482a1a411a9f364fcfdbe134a82f0ce8e2256d3
aihridoy <cse_1912020168@lus.ac.bd>
2025-09-05 04:12:32 +0600

    fix: navbar responsiveness

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,114 +1,165 @@
 import { Link } from 'react-router-dom';
 import { Avatar, AvatarFallback, AvatarImage } from '../components/ui/avatar';
 import { Button } from '../components/ui/button';
-import { DropdownMenuSeparator } from '../components/ui/dropdown-menu';
-import { DropdownMenuLabel } from '../components/ui/dropdown-menu';
-
 import {
-  DropdownMenuItem,
   DropdownMenu,
   DropdownMenuTrigger,
   DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
 } from '../components/ui/dropdown-menu';
-import { HiOutlineSearch } from 'react-icons/hi';
+import { HiOutlineSearch, HiOutlineMenu, HiX } from 'react-icons/hi';
 import Cart from '../components/Cart';
 import logo from '../assets/images/technet-logo.png';
 import { useAppDispatch, useAppSelector } from '@/redux/hook';
 import { signOut } from 'firebase/auth';
 import { auth } from '@/lib/firebase';
 import { setUser } from '@/redux/features/user/userSlice';
+import { useState } from 'react';
 
 export default function Navbar() {
   const { user } = useAppSelector((state) => state.user);
   const dispatch = useAppDispatch();
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   const handleLogout = () => {
     signOut(auth).then(() => {
       dispatch(setUser(null));
     });
   };
+
   return (
     <nav className="w-full h-16 fixed top backdrop-blur-lg z-50">
       <div className="h-full w-full bg-white/60">
-        <div className="flex items-center justify-between w-full md:max-w-7xl h-full mx-auto ">
+        <div className="flex items-center justify-between w-full md:max-w-7xl h-full mx-auto px-4">
+          {/* Logo */}
           <Link to="/">
-            <img className="h-8" src={logo} alt="log" />
+            <img className="h-8" src={logo} alt="logo" />
           </Link>
-          <div>
-            <ul className="flex items-center">
-              <li>
-                <Button variant="link" asChild>
-                  <Link to="/">Home</Link>
-                </Button>
-              </li>
-              <li>
-                <Button variant="link" asChild>
-                  <Link to="/products">Products</Link>
-                </Button>
-              </li>
-              <li>
-                <Button variant="link" asChild>
-                  <Link to="/checkout">Checkout</Link>
-                </Button>
-              </li>
-              <li>
-                <Button variant="ghost">
-                  <HiOutlineSearch size="25" />
-                </Button>
-              </li>
-              <li>
-                <Cart />
-              </li>
-              <li className="ml-5">
-                <DropdownMenu>
-                  <DropdownMenuTrigger className="outline-none">
-                    <Avatar>
-                      <AvatarImage src="https://github.com/shadcn.png" />
-                      <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent>
-                    <DropdownMenuLabel>Account</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem className="cursor-pointer">
-                      Profile
+
+          {/* Desktop Menu */}
+          <ul className="hidden md:flex items-center space-x-1">
+            <li>
+              <Button variant="link" asChild>
+                <Link to="/">Home</Link>
+              </Button>
+            </li>
+            <li>
+              <Button variant="link" asChild>
+                <Link to="/products">Products</Link>
+              </Button>
+            </li>
+            <li>
+              <Button variant="link" asChild>
+                <Link to="/checkout">Checkout</Link>
+              </Button>
+            </li>
+            <li>
+              <Button variant="ghost">
+                <HiOutlineSearch size="22" />
+              </Button>
+            </li>
+            <li>
+              <Cart />
+            </li>
+            <li className="ml-5">
+              <DropdownMenu>
+                <DropdownMenuTrigger className="outline-none">
+                  <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Profile</DropdownMenuItem>
+                  <DropdownMenuItem>Billing</DropdownMenuItem>
+                  <DropdownMenuItem>Team</DropdownMenuItem>
+                  <DropdownMenuItem>Subscription</DropdownMenuItem>
+                  {!user.email && (
+                    <>
+                      <Link to="/login">
+                        <DropdownMenuItem>Login</DropdownMenuItem>
+                      </Link>
+                      <Link to="/signup">
+                        <DropdownMenuItem>Signup</DropdownMenuItem>
+                      </Link>
+                    </>
+                  )}
+                  {user.email && (
+                    <DropdownMenuItem onClick={handleLogout}>
+                      Logout
                     </DropdownMenuItem>
-                    <DropdownMenuItem className="cursor-pointer">
-                      Billing
-                    </DropdownMenuItem>
-                    <DropdownMenuItem className="cursor-pointer">
-                      Team
-                    </DropdownMenuItem>
-                    <DropdownMenuItem className="cursor-pointer">
-                      Subscription
-                    </DropdownMenuItem>
-                    {!user.email && (
-                      <>
-                        <Link to="/login">
-                          <DropdownMenuItem className="cursor-pointer">
-                            Login
-                          </DropdownMenuItem>
-                        </Link>
-                        <Link to="/signup">
-                          <DropdownMenuItem className="cursor-pointer">
-                            Signup
-                          </DropdownMenuItem>
-                        </Link>
-                      </>
-                    )}
-                    {user.email && (
-                      <DropdownMenuItem
-                        onClick={handleLogout}
-                        className="cursor-pointer"
-                      >
-                        Logout
-                      </DropdownMenuItem>
-                    )}
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </li>
-            </ul>
-          </div>
+                  )}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </li>
+          </ul>
+
+          {/* Mobile Menu Button */}
+          <button
+            className="md:hidden p-2 rounded-lg hover:bg-gray-200"
+            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+          >
+            {mobileMenuOpen ? <HiX size={24} /> : <HiOutlineMenu size={24} />}
+          </button>
         </div>
+
+        {/* Mobile Menu Dropdown */}
+        {mobileMenuOpen && (
+          <div className="md:hidden bg-white/95 backdrop-blur-md shadow-md px-4 py-3 space-y-3">
+            <Link to="/" onClick={() => setMobileMenuOpen(false)}>
+              <Button variant="link" className="w-full justify-start">
+                Home
+              </Button>
+            </Link>
+            <Link to="/products" onClick={() => setMobileMenuOpen(false)}>
+              <Button variant="link" className="w-full justify-start">
+                Products
+              </Button>
+            </Link>
+            <Link to="/checkout" onClick={() => setMobileMenuOpen(false)}>
+              <Button variant="link" className="w-full justify-start">
+                Checkout
+              </Button>
+            </Link>
+            <div className="flex items-center space-x-3">
+              <Button variant="ghost">
+                <HiOutlineSearch size={22} />
+              </Button>
+              <Cart />
+            </div>
+            <div className="border-t pt-3">
+              {!user.email ? (
+                <>
+                  <Link to="/login" onClick={() => setMobileMenuOpen(false)}>
+                    <Button variant="outline" className="w-full">
+                      Login
+                    </Button>
+                  </Link>
+                  <Link to="/signup" onClick={() => setMobileMenuOpen(false)}>
+                    <Button variant="default" className="w-full mt-2">
+                      Signup
+                    </Button>
+                  </Link>
+                </>
+              ) : (
+                <Button
+                  onClick={() => {
+                    handleLogout();
+                    setMobileMenuOpen(false);
+                  }}
+                  variant="destructive"
+                  className="w-full"
+                >
+                  Logout
+                </Button>
+              )}
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
